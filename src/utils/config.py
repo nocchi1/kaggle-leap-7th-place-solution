5abb34c982e4ccda7f183041ac9763deada88fb0
nocchi1 <>
2024-10-01 01:46:50 +0000

    add config yaml

--- src/utils/config.py
+++ src/utils/config.py
@@ -3,10 +3,10 @@ from pathlib import Path, PosixPath
 from omegaconf import DictConfig, OmegaConf
 
 
-def get_config(exp: str, config_dir: PosixPath = Path("../config")) -> DictConfig:
+def get_config(exp: str, config_dir: PosixPath = Path("./config")) -> DictConfig:
     OmegaConf.register_new_resolver("path", lambda x: Path(x), replace=True)
-    global_config = OmegaConf.load(config_dir / "global.yml")
-    exp_config = OmegaConf.load(config_dir / f"exp_{exp}.yml")
+    global_config = OmegaConf.load(config_dir / "global.yaml")
+    exp_config = OmegaConf.load(config_dir / f"exp_{exp}.yaml")
     config = OmegaConf.merge(global_config, exp_config)
 
     config.output_path = config.output_path / exp
